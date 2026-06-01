78ceca60a849c7ac66b5f11b37acfadc4529c85e
Robert Ocran <robertocran97@gmail.com>
2026-06-01 22:01:16 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,36 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Robert Kwesi Ocran
+# STUDENT ID: 2425404096
+# Email: 2425404096@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Robert Kwesi Ocran, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
 
 def main():
-  pass #code goes here
+    # Print my initials in normal and large form.
 
+    print()
+    print("...ROK")
+    print()
+
+    print("...RRRRRRRRRR...........OOOOOOOOOO.........KKK......KKK.....")
+    print("...RRR.....RRR.........OOOOOOOOOOOO........KKK.....KKK......")
+    print("...RRR......RRR........OOO......OOO........KKK....KKK.......")
+    print("...RRR......RRR........OO........OO........KKK..KKK.........")
+    print("...RRR....RRR..........OO........OO........KKK.KKK..........")
+    print("...RRRRRRRR............OO........OO........KKK.KKK..........")
+    print("...RRR...RRR...........OO........OO........KKK..KKK.........")
+    print("...RRR.....RRR.........OOO......OOO........KKK....KKK.......")
+    print("...RRR......RRR...**...OOOOOOOOOOOO...**...KKK......KKK...**")
+    print("...RRR......RRR...**....OOOOOOOOOO....**...KKK......KKK...**")
+
+    print()
 
 
 main()
